25490dddd222b2281c82c7e593544445a4c34cb7
Roman <poma.komarow@gmail.com>
2020-02-13 22:19:27 +0300

    Revert "Taks1"
    This reverts commit 335c8fa034ba9e211e29cf113ebcb61ece96c0aa.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { LoginComponent } from './login/login.component';
-import { SearchingResultsComponent } from './searching-results/searching-results.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { CardComponent } from './searching-results/card/card.component';
-import { CardInfoComponent } from './card-info/card-info.component';
-import { SerchComponent } from './shared/header/serch/serch.component';
-import { AuthComponent } from './shared/header/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SearchingResultsComponent,
-    NotFoundComponent,
-    CardComponent,
-    CardInfoComponent,
-    SerchComponent,
-    AuthComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderComponent {
-  constructor() {}
-}
