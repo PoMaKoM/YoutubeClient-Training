98eecb7627f866317587ae11f779250c5b5fbf99
Roman <poma.komarow@gmail.com>
2020-02-20 14:54:04 +0300

    Task1 v2.0

---
+++ src/app/components/header/auth-info/auth-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-info',
+  templateUrl: './auth-info.component.html',
+  styleUrls: ['./auth-info.component.scss']
+})
+export class AuthInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
